d1af35e4f3d3398c9de00b546149bb8565fd9db0
Nina Vasilieva <aninok92@gmail.com>
2021-08-26 11:34:01 +0300

    добавляет export userSelectors

--- src/redux/users/index.js
+++ src/redux/users/index.js
@@ -1,2 +1,3 @@
 export { default as userOperations } from "./users-operations";
+export { default as userSelectors } from "./users-selectors";
 export { default as userReducer } from "./users-slice";
